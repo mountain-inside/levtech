9a2a6010e5e8313987914ae67ae4d4bae9f07a4a
mountain-inside <ce245034@meiji.ac.jp>
2024-07-19 16:19:30 +0900

    postコントローラーとモデルの設定

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+  //外部にあるPostControllerクラスをインポート。
+
+
+Route::get('/posts', [PostController::class, 'index']);   
